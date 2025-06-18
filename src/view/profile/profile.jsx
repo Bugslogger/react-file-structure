b74f0bcc7bb8cedace3134cc1396e00db9e007ec
sarfaraj <sarfaraj.s@nissisoftwaresystems.com>
2025-06-18 15:41:56 +0530

    added models

--- src/view/profile/profile.jsx
+++ src/view/profile/profile.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Error from "../../components/models/error";
+import Success from "../../components/models/success";
+import Warning from "../../components/models/warning";
 
 const Profile = () => {
-  return <div></div>;
+  const [open, setopen] = useState(false);
+  return (
+    <>
+      <button onClick={() => setopen(true)}>Click</button>
+      <Warning handleClose={() => setopen(false)} open={open} isButton isCloseButton isIcon />
+    </>
+  );
 };
 
 export default Profile;
