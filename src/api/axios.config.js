94d914e5213b899b6e39e787d949024d39a337c2
sarfaraj <sarfaraj.s@nissisoftwaresystems.com>
2025-06-19 13:26:38 +0530

    updated interceptor

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -5,25 +5,33 @@ const cookie = new Cookies(null, { path: "/", sameSite: "strict" });
 
 const api = axios.create({ baseURL: BASE_URL, timeout: 20000 });
 
+// default configuration
+api.defaults.withCredentials = true;
+api.defaults.headers.common["Content-Type"] = "application/json";
+
 const interceptor = api.interceptors.request.use((config) => {
-  const token = cookie.get("token");
+  const token = cookie.get("token"); // you need to change cookie name according to what you have set while setting cookie to browser.
 
   // ✅ Modify request config before sending
   console.log("Outgoing Request:", config);
 
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+  // Only attach Authorization header if config.withAuth is true
+  if (config.withCredentials) {
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
   }
 
   // ⚠️ Do NOT set Content-Type manually for FormData
   if (config.data instanceof FormData) {
     // Let the browser set the correct boundary in multipart/form-data
+    config.headers["Content-Type"] = "multipart/form-data";
   }
   return config;
 });
 
 const responseInterceptor = api.interceptors.response.use((config) => {
-  console.log(config);
+  console.log("Response Interceptor: ", config);
 
   return config;
 });
